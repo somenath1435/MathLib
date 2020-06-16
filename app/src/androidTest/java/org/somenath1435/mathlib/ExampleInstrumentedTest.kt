14c5d081ea8f0c6df52c71be395c7c36c73840ad
somenath1435 <somenath1435@gmail.com>
2020-06-16 12:10:40 +0530

    fixed typo

--- app/src/androidTest/java/org/somenath1435/mathlib/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/org/somenath1435/mathlib/ExampleInstrumentedTest.kt
@@ -20,6 +20,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("org.somenath1435.mathlib.test", appContext.packageName)
+        assertEquals("org.somenath1435.mathlibs.test", appContext.packageName)
     }
 }
